c93abd21d2a1bdb42f7872472f3b0e3c97a889b1
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-05-04 21:15:52 +0300

    Add files via upload

---
+++ lessons005/script.js
@@ -0,0 +1,73 @@
+// 1)
+// let radius=+prompt('Введите радиус окружности');
+
+// alert(`Площадь окружности с радиусом ${radius} равна ${3.14*(radius**2)}`)
+
+// 2)
+// let age=+prompt('Введите свой возраст');
+// let year=2023
+//  for (let i=1; i<10; i++){
+//     console.log(`В ${year+i} году будет ${age+i} лет`)
+//  }
+
+
+// 2) второй вариант
+// let carrentAge=+prompt('Введите свой возраст');
+// let carrentYear=2024;
+//  const maxYear=2030;
+ 
+//  while (carrentYear<maxYear){
+//     console.log(`В ${carrentYear} году будет ${carrentAge} лет`);
+//     carrentAge++;
+//     carrentYear++
+//  }
+
+// 3) 
+// let start=40;
+// let end=140;
+
+// while(start<=end){
+//     if (start%2!==0){
+//     console.log(start);
+//     }
+        
+//     start++;
+// }
+
+// 4)
+// let otr=0;
+// let pol=0;
+// let zero=0;
+
+
+
+// for (let i=0; i<=10;i++){
+//     let num=+prompt('Введите число');
+
+//     if (num<0){
+//         otr++;
+//     }
+//     else if (num>0){
+//         pol++;
+//     }
+//     else {
+//         zero++;
+//     }
+// }
+// console.log(`Положительных: ${otr}
+//             Отрицательных: ${pol}
+//             Нулей: ${zero}`);
+
+// 5)
+
+// const simbol='#';
+
+// let resultString='';
+// let i=1;
+// let userNumber=+prompt('Введите число');
+
+// while (i<=userNumber){
+//     resultString+=simbol;
+//     i++;
+// }
+// console.log(resultString);
