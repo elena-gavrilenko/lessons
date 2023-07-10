786b05b06ebca8686a6a02b3f668548f34a38784
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-07-10 21:18:00 +0300

    Add files via upload

---
+++ lessons017_3_project/script.js
@@ -0,0 +1,5 @@
+const counter=document.querySelector('.counter');
+const dec=document.querySelector('.dec');
+const reset=document.querySelector('.reset');
+const inc=document.querySelector('.inc');
+
