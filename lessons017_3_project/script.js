bf290f82404d2fca5e46f1576a63e8b06a1d4a81
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-07-11 16:59:46 +0300

    Delete lessons017_3_project directory

--- lessons017_3_project/script.js
+++
@@ -1,5 +0,0 @@
-const counter=document.querySelector('.counter');
-const dec=document.querySelector('.dec');
-const reset=document.querySelector('.reset');
-const inc=document.querySelector('.inc');
-
