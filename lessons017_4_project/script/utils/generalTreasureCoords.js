16b1d3a1d45f72da65711058e310edf504fb30c8
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-07-26 18:02:58 +0300

    Delete lessons017_4_project directory

--- lessons017_4_project/script/utils/generalTreasureCoords.js
+++
@@ -1,8 +0,0 @@
-import { generalRAndomNumber } from "./generalRandomNumber.js"
-
-export const generalTreasureCoords=({width,height},gap)=>({
-// gap - отступ
-// width - ширина карты , height - высота карты
-        x: generalRAndomNumber(width,gap),
-        y: generalRAndomNumber(height,gap)
-})
