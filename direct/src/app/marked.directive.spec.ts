075be19aee437b1096e4fb6f6b25a1547bac973f
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-10-24 19:54:00 +0300

    Delete direct/src directory

--- direct/src/app/marked.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkedDirective } from './marked.directive';
-
-describe('MarkedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MarkedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
