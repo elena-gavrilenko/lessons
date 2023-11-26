e3c5117dc7f8e20471c27b5f16b14c0f2e582e44
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2023-11-26 19:12:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+  <React.Fragment>
+    <h2>My first app</h2>
+    <h3>second</h3>
+  </React.Fragment>
+  );
+ 
+}
+
+export default App;
+ // второй способ вставки компонент <React.Fragment> для этого нужно импортировать React
+  /* <>    первый способ вставки компонент*/
