5d8c1dcb1da174948b18109078b2ea6fa6f4afff
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2024-01-11 20:30:41 +0300

    Delete short2 directory

--- short2/src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { todoReducer } from "./todoReducer";
-
-export const store = createStore(todoReducer,composeWithDevTools())
--- short2/src/redux/todoSelectors.js
+++
@@ -1,6 +0,0 @@
-export const getTodos = (state) => state.todos
-
-export const getFilterType=(state)=>state.filterType
-
-
-// export const getTodos = ({todos}) => todos
