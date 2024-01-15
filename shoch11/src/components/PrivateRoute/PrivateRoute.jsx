49526b85cfd103dbc7e46c9cebe939fbf522c5dc
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2024-01-15 17:36:55 +0300

    Delete shoch11 directory

--- shoch11/src/components/PrivateRoute/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { getIsUserAuth } from "../../redux/user/userSelectors"
-
-export const PrivateRoute = (props) => {
-    const isAuth = useSelector(getIsUserAuth)
-    return isAuth ? props.children : <Navigate to='/' />
-
-}
--- shoch11/src/components/constants/api.js
+++
@@ -1,1 +0,0 @@
-export const API_URL_LOGIN='https://dummyjson.com/auth/login'
