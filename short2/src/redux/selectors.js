9a6b166951fe219318358513be708780617c71f4
elena-gavrilenko <71605937+elena-gavrilenko@users.noreply.github.com>
2024-02-15 14:15:28 +0300

    Delete short2 directory

--- short2/src/redux/selectors.js
+++
@@ -1,5 +0,0 @@
-// export const getCounterValue = (state) => {
-//     return state.counter
-// }
-
-export const getCounterValue = state => state.counter
--- short2/src/toolkit/slices/selector.js
+++
@@ -1,6 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-const selectCounter=state=>state.counter
-export const getCurrentValueToolkin=createSelector(selectCounter,(state)=>{
-    return state.count
-})
